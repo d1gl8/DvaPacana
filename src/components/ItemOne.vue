2f5b8274b792165fe3b5ed6b077ce0f4d435debc
d1gl8 <d1gl8@ya.ru>
2024-12-11 10:06:07 +0300

    test

---
+++ src/components/ItemOne.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps<{ item?: Item }>()
+</script>
+
+<template>
+  <div class="item">
+    {{ item?.name }}
+  </div>
+</template>
+
+<style>
+.item {
+  height: 40px;
+  display: grid;
+  place-items: center;
+  cursor: pointer;
+  background-color: white;
+  padding: 10px;
+}
+</style>
